624c50133dc80e13ecd7cdb4b69d1d5c1a64c2cf
Alessandro Avolio <social@avol.io>
2026-04-18 20:10:16 +0200

    chore: publish

---
+++ docs/assets/stateDiagram-v2-QKLJ7IA2-CMrEYPoJ.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-OYMX7WX6-B5wGK4Eh.js";import{_ as i}from"./index-CeWn1qoG.js";import"./chunk-55IACEB6-Chv55PE6.js";import"./chunk-EDXVE4YY-BJ2AxQkm.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
